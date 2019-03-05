1a3d03f058976367648d7ca64b224f2ef21f5827
Ryan Dean <ryan@mobalyticshq.com>
2019-03-05 10:23:07 -0700

    feature: added REST server

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.8
+FROM golang:1.9
 
 RUN mkdir -p /go/src/redeam-rest
 WORKDIR /go/src/redeam-rest
@@ -6,4 +6,4 @@ WORKDIR /go/src/redeam-rest
 ADD . /go/src/redeam-rest
 RUN mkdir -p /docker-entrypoint-initdb.d
 WORKDIR /go/src/redeam-rest/cmd/server
-RUN go get -v
+RUN go get
--- pkg/cmd/server.go
+++ pkg/cmd/server.go
@@ -3,49 +3,24 @@ package cmd
 import (
 	"context"
 	"database/sql"
-	"flag"
 	"fmt"
 
 	_ "github.com/lib/pq"
 
 	"github.com/radean0909/redeam-rest/pkg/protocol/grpc"
+	"github.com/radean0909/redeam-rest/pkg/protocol/rest"
 	"github.com/radean0909/redeam-rest/pkg/service/v1"
 )
-
-type Config struct {
-	GRPCPort string
-
-	PgDBHost     string
-	PgDBUser     string
-	PgDBPassword string
-	PgDBSchema   string
-}
 
 // RunServer runs gRPC server and HTTP gateway
 func RunServer() error {
 	ctx := context.Background()
 
-	// get configuration
-	var cfg Config
-	flag.StringVar(&cfg.GRPCPort, "grpc-port", "", "gRPC port to bind")
-	flag.StringVar(&cfg.PgDBHost, "db-host", "", "Database host")
-	flag.StringVar(&cfg.PgDBUser, "db-user", "", "Database user")
-	flag.StringVar(&cfg.PgDBPassword, "db-password", "", "Database password")
-	flag.StringVar(&cfg.PgDBSchema, "db-schema", "", "Database schema")
-	flag.Parse()
-
-	if len(cfg.GRPCPort) == 0 {
-		return fmt.Errorf("invalid TCP port for gRPC server: '%s'", cfg.GRPCPort)
-	}
-
-	param := "parseTime=true"
-
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?%s",
-		cfg.PgDBUser,
-		cfg.PgDBPassword,
-		cfg.PgDBHost,
-		cfg.PgDBSchema,
-		param)
+		"postgres-dev",
+		"sn34kyp4$$w0rD",
+		"0.0.0.0",
+		"redeam-library")
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return fmt.Errorf("failed to open database: %v", err)
@@ -54,5 +29,10 @@ func RunServer() error {
 
 	v1API := v1.NewBookServiceServer(db)
 
-	return grpc.RunServer(ctx, v1API, cfg.GRPCPort)
+	// run HTTP gateway
+	go func() {
+		_ = rest.RunServer(ctx, "9090", "8080")
+	}()
+
+	return grpc.RunServer(ctx, v1API, "9090")
 }
